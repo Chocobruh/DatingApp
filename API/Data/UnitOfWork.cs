dda42f19e648988ea44319947e708b57ad441b18
Chocobruh <olteanufabian99@gmail.com>
2021-09-08 17:42:16 +0300

    preswitch to postgres

--- API/Data/UnitOfWork.cs
+++ API/Data/UnitOfWork.cs
@@ -8,17 +8,38 @@ namespace API.Data
     {
         private readonly IMapper _mapper;
         private readonly DataContext _context;
+        private IUserRepository _userRepository;
+        private IMessageRepository _messageRepository;
+        private ILikesRepository _likesRepository;
         public UnitOfWork(DataContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        public IUserRepository UserRepository => new UserRepository(_context, _mapper);
+        public IUserRepository UserRepository 
+        {
+            get
+            {
+                return _userRepository ??= new UserRepository(_context, _mapper);
+            }
+        }
 
-        public IMessageRepository MessageRepository => new MessageRepository(_context, _mapper);
+        public IMessageRepository MessageRepository
+        {
+            get
+            {  
+                return _messageRepository ??= new MessageRepository(_context, _mapper);
+            }
+        }
 
-        public ILikesRepository LikesRepository => new LikesRepository(_context);
+        public ILikesRepository LikesRepository
+        {
+            get
+            {
+                return _likesRepository ??= new LikesRepository(_context);
+            }
+        }
 
         public async Task<bool> Complete()
         {
